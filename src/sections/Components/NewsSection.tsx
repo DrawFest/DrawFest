724b3a84d57149983eff12c79b6e5e3bd3e842fd
DrawFest <markiwhite0225@gmail.com>
2025-11-20 14:13:37 -0600

    Create NewsSection.tsx

---
+++ src/sections/Components/NewsSection.tsx
@@ -0,0 +1,12 @@
+export const NewsSection = () => {
+  return (
+    <h2 className="text-[28.8px] font-medium box-border caret-transparent leading-[34.56px] break-words mb-2">
+      <a
+        href="news"
+        className="text-blue-300 box-border caret-transparent break-words hover:text-teal-400 hover:underline hover:border-teal-400"
+      >
+        News
+      </a>
+    </h2>
+  );
+};

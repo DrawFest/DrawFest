a22f7f526fac759b4b068787ccf16418dcfb1e7e
DrawFest <markiwhite0225@gmail.com>
2025-11-20 14:07:26 -0600

    Update Index.tsx

--- src/sections/HeroBanner/Index.tsx
+++ src/sections/HeroBanner/Index.tsx
@@ -5,7 +5,7 @@ export const HeroBanner = () => {
         href="/"
         className="text-blue-300 box-border caret-transparent hover:text-teal-400 hover:underline hover:border-teal-400"
       >
-        <div className="bg-[url('https://artfight.net/images/default_banner.jpg')] bg-cover box-border caret-transparent h-[200px] bg-[position:50%_top]"></div>
+        <div className="bg-[url('https://f2.toyhou.se/file/f2-toyhou-se/images/107424410_Vxj1Us8IrgXWMIr.png')] bg-cover box-border caret-transparent h-[200px] bg-[position:50%_top]"></div>
       </a>
     </div>
   );

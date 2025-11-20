710552e4883f86c2fe0eb9e2958add3d3ff28535
DrawFest <markiwhite0225@gmail.com>
2025-11-20 14:12:53 -0600

    Create NavigationButtons.tsx

---
+++ src/sections/Components/NavigationButtons.tsx
@@ -0,0 +1,51 @@
+export const NavigationButtons = () => {
+  return (
+    <div className="box-border caret-transparent flex flex-wrap ml-[-15px] mr-[-15px] break-words">
+      <div className="relative box-border caret-transparent basis-auto grow-0 shrink max-w-none break-words w-full mb-4 px-[15px] md:basis-0 md:grow md:shrink-0 md:max-w-full">
+        <a
+          type="button"
+          href="https://artfight.net/info/about"
+          className="text-white text-lg bg-slate-500 box-border caret-transparent block leading-[27px] break-words text-center align-middle w-full border border-slate-500 px-4 py-2 rounded-[4.8px] border-solid hover:bg-cyan-800 hover:border-cyan-800"
+        >
+          How It Works
+        </a>
+      </div>
+      <div className="relative box-border caret-transparent basis-auto grow-0 shrink max-w-none break-words w-full mb-4 px-[15px] md:basis-0 md:grow md:shrink-0 md:max-w-full">
+        <a
+          type="button"
+          href="https://artfight.net/info/rules"
+          className="text-white text-lg bg-slate-500 box-border caret-transparent block leading-[27px] break-words text-center align-middle w-full border border-slate-500 px-4 py-2 rounded-[4.8px] border-solid hover:bg-cyan-800 hover:border-cyan-800"
+        >
+          Site Rules
+        </a>
+      </div>
+      <div className="relative box-border caret-transparent basis-auto grow-0 shrink max-w-none break-words w-full mb-4 px-[15px] md:basis-0 md:grow md:shrink-0 md:max-w-full">
+        <a
+          type="button"
+          href="https://artfight.net/info/guide-attacks"
+          className="text-white text-lg bg-slate-500 box-border caret-transparent block leading-[27px] break-words text-center align-middle w-full border border-slate-500 px-4 py-2 rounded-[4.8px] border-solid hover:bg-cyan-800 hover:border-cyan-800"
+        >
+          Attack Guide
+        </a>
+      </div>
+      <div className="relative box-border caret-transparent basis-auto grow-0 shrink max-w-none break-words w-full mb-4 px-[15px] md:basis-0 md:grow md:shrink-0 md:max-w-full">
+        <a
+          type="button"
+          href="https://artfight.net/info/guide-filters"
+          className="text-white text-lg bg-slate-500 box-border caret-transparent block leading-[27px] break-words text-center align-middle w-full border border-slate-500 px-4 py-2 rounded-[4.8px] border-solid hover:bg-cyan-800 hover:border-cyan-800"
+        >
+          Filters Guide
+        </a>
+      </div>
+      <div className="relative box-border caret-transparent basis-auto grow-0 shrink max-w-none break-words w-full mb-4 px-[15px] md:basis-0 md:grow md:shrink-0 md:max-w-full">
+        <a
+          type="button"
+          href="https://artfight.net/info/faq"
+          className="text-white text-lg bg-slate-500 box-border caret-transparent block leading-[27px] break-words text-center align-middle w-full border border-slate-500 px-4 py-2 rounded-[4.8px] border-solid hover:bg-cyan-800 hover:border-cyan-800"
+        >
+          FAQ
+        </a>
+      </div>
+    </div>
+  );
+};

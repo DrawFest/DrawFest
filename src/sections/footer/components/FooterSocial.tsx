aee670e16703f1fc069e91142867b6d84a336ae6
DrawFest <markiwhite0225@gmail.com>
2025-11-20 14:00:34 -0600

    Create FooterSocial.tsx

---
+++ src/sections/footer/components/FooterSocial.tsx
@@ -0,0 +1,62 @@
+export const FooterSocial = () => {
+  return (
+    <div className="text-[28.8px] box-border caret-transparent leading-[43.2px]">
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://twitter.com/theartfight"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://www.instagram.com/artfight"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://twitch.tv/theartfight"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://tumblr.com/artfight"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://www.tiktok.com/@theartfight"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://www.youtube.com/@ArtFightOfficial"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+      <div className="box-border caret-transparent pr-[14.4px]">
+        <a
+          href="https://bsky.app/profile/artfight.net"
+          className="text-stone-300 box-border caret-transparent hover:text-white hover:underline hover:border-white"
+        >
+          <i className="box-border caret-transparent inline-block leading-[28.8px] font-font_awesome_6_brands before:accent-auto before:box-border before:caret-transparent before:text-stone-300 before:text-[28.8px] before:not-italic before:normal-nums before:font-normal before:tracking-[normal] before:leading-[28.8px] before:list-outside before:list-disc before:pointer-events-auto before:text-left before:indent-[0px] before:normal-case before:visible before:border-separate before:font-font_awesome_6_brands"></i>
+        </a>
+      </div>
+    </div>
+  );
+};

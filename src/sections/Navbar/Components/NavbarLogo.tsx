e69cb6814e86191c6a8b7203bc93ea9e071b4ab9
DrawFest <markiwhite0225@gmail.com>
2025-11-20 14:20:04 -0600

    Create NavbarLogo.tsx

---
+++ src/sections/Navbar/Components/NavbarLogo.tsx
@@ -0,0 +1,16 @@
+export const NavbarLogo = () => {
+  return (
+    <a
+      href="/"
+      className="text-white text-lg items-center box-border caret-transparent flex leading-[27px] text-nowrap mr-4"
+    >
+      <img
+        src="https://c.animaapp.com/mi7ttux6JpWCPI/assets/logo40.png"
+        className="box-border caret-transparent max-h-[1000px] max-w-full min-w-5 text-nowrap w-3/12 mr-2"
+      />
+      <div className="box-border caret-transparent text-nowrap w-9/12">
+        DrawFest
+      </div>
+    </a>
+  );
+};
